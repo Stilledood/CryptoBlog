bfa50b09ce75a9ad614e0aaa471e1e81b9c76f34
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-01-08 21:16:23 +0200

    commit 3

--- cryptoblog/blog/models.py
+++ cryptoblog/blog/models.py
@@ -13,8 +13,9 @@ class Post(models.Model):
     tag=TaggableManager()
 
 
+
     class Meta:
-        ordering='-date_added'
+        ordering=('-date_added',)
         get_latest_by='date_added'
 
 
@@ -30,6 +31,9 @@ class Post(models.Model):
     def get_delete_url(self):
         return reverse('post_delete',kwargs={'pk':self.pk})
 
+    def get_comment_url(self):
+        return reverse('comment_create',kwargs={'pk':self.pk})
+
 
 
 
@@ -37,12 +41,12 @@ class Answer(models.Model):
     '''Class to construct models for creating answer objects'''
 
     body=models.TextField()
-    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
     date_added=models.DateField(auto_now_add=True)
     post=models.ForeignKey(Post,on_delete=models.CASCADE)
 
     class Meta:
-        ordering='-date_added'
+        ordering=('-date_added',)
         get_latest_by='date_added'
 
     def __str__(self):
@@ -54,23 +58,6 @@ class Answer(models.Model):
         return reverse('answer_delete',kwargs={'pk1':self.post.pk,'pk2':self.pk})
 
 
-class Reply(models.Model):
-    '''Class to construct models for creating reply objects'''
 
-    body=models.TextField()
-    date_added=models.DateField(auto_now_add=True)
-    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    answer=models.ForeignKey(Answer,on_delete=models.CASCADE)
-
-
-    class Meta:
-        ordering='-date_added'
-        get_latest_by='date_added'
-
-    def __str__(self):
-        return self.body[:20]
-
-    def get_delete_url(self):
-        return reverse('reply_delete',kwargs={'pk1':self.answer.post.pk,'pk2':self.answer.pk,'pk3':self.pk})
 
 
--- cryptoblog/blog/views.py
+++ cryptoblog/blog/views.py
@@ -1,3 +1,134 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import Post,Answer
+from .forms import PostFrom,CommentForm
+from django.views.generic import View
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
-# Create your views here.
+
+class PostList(View):
+    '''Class to construct a view for list of post objects'''
+
+    model=Post
+    template_name='blog/post_list.html'
+    page_kwargs='page'
+    paginated_by=2
+
+    def get(self,request):
+        post_list=self.model.objects.all()
+        paginator=Paginator(post_list,self.paginated_by)
+        page_number=request.GET.get(self.page_kwargs)
+        try:
+            page=paginator.page(page_number)
+        except PageNotAnInteger:
+            page=paginator.page(1)
+        except EmptyPage:
+            page=paginator.page(paginator.num_pages)
+
+        if page.has_previous():
+            previous_page_url="?{pkw}={n}".format(pkw=self.page_kwargs,n=page.previous_page_number())
+        else:
+            previous_page_url=None
+
+        if page.has_next():
+            next_page_url="?{pkw}={n}".formst(pkw=self.page_kwargs,n=page.next_page_number())
+        else:
+            next_page_url=None
+
+        context={
+            'is_paginated':page.has_other_pages(),
+            'previous_page_url':previous_page_url,
+            'next_page_url':next_page_url,
+            'paginator':paginator,
+            'post_list':page
+        }
+
+        return render(request,self.template_name,context)
+
+
+
+
+class PostDetails(View):
+    '''Class to construct a view for details of each post object'''
+
+    model=Post
+    template_name='blog/post_details.html'
+
+    def get(self,request,pk):
+        post=get_object_or_404(self.model,pk=pk)
+        form=CommentForm()
+        return render(request,self.template_name,{'post':post,'form':form})
+
+    def post(self,request,pk):
+        post=get_object_or_404(self.model,pk=pk)
+        form=CommentForm(request.POST)
+        if form.is_valid():
+            comment=form.save(commit=False)
+            comment.post=post
+            comment.save()
+            return redirect(post.get_absolute_url())
+        else:
+            form=CommentForm()
+            return render(request,self.template_name,{'form':form})
+
+
+
+
+
+
+
+
+
+
+class PostCreate(View):
+    '''Class to create views for creating objects'''
+
+    model=Post
+    form_class=PostFrom
+    template_name='blog/post_create.html'
+
+    def get(self,request):
+        return render(request,self.template_name,{'form':self.form_class()})
+
+    def post(self,request):
+        bound_form=self.form_class(request.POST)
+        if bound_form.is_valid():
+            new_post=bound_form.save()
+            return redirect('post_list')
+        else:
+            return render(request,self.template_name,{'form':bound_form})
+
+
+
+class CommentCreate(View):
+    '''Class to create a view for creating answer objects'''
+
+    model=Answer
+    form_class=CommentForm
+    template_name='blog/post_details.html'
+
+    def get(self,request,pk):
+        post=get_object_or_404(Post,pk=pk)
+        return render(request,self.template_name,{'form':self.form_class()})
+
+    def post(self,request,pk):
+        post=get_object_or_404(Post,pk=pk)
+        bound_form=self.form_class(request.POST)
+        if bound_form.is_valid():
+            new_comment=bound_form.save(commit=False)
+            new_comment.post=post
+            new_comment.save()
+            return redirect(post.get_absolute_url())
+        else:
+            return render(request,self.template_name,{'form':bound_form})
+
+
+
+
+
+
+
+
+
+
+
+
